0f2a71ed66eb09c5b8a90cb7fdcd8fb4c6c82f4a
Brad Jordan <climberbrad@users.noreply.github.com>
2022-09-22 16:04:28 -0600

    useReduce only

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,59 @@
-import React, {useReducer} from 'react';
+import React, {useReducer, useRef} from 'react';
 import './App.css';
 
 export const buttonCss = 'bg-blue-400 border border-gray-100 mx-2 px-2 rounded text-white hover:bg-gray-300 hover:text-black font-semibold'
 
+// every possible action is here!
+enum actionEnum {
+    INCREMENT,
+    DECREMENT,
+    RESET,
+    UPDATE_INCREMENTOR
+}
+
+// finite type of event for this component
+interface actionEvent {
+    type: actionEnum,
+    incrementor: number,
+    value: number,
+}
+
+const MIN_INCREMENT = 1;
+
+// known empty state
+const defaultValue = {
+    type: actionEnum.INCREMENT,
+    incrementor: MIN_INCREMENT,
+    value: 0,
+}
+
+
+// all business logic for the entire component is in here. All state is tracked in one spot.
+const myReducer = (state: actionEvent, action: actionEvent): actionEvent => {
+    switch(action.type) {
+        case actionEnum.INCREMENT:
+            return { ...state, value: action.value + action.incrementor};
+        case actionEnum.DECREMENT:
+            return { ...state, value: action.value - action.incrementor};
+        case actionEnum.RESET:
+            return { ...state, value: 0};
+        case actionEnum.UPDATE_INCREMENTOR:
+            return { ...state, incrementor: action.incrementor <= 0 ? MIN_INCREMENT : action.incrementor};
+        default: return state;
+    }
+}
+
 function App() {
+    const textInputRef = useRef<HTMLInputElement>(null);
+    const [count, dispatch] = useReducer(myReducer, defaultValue)
 
-    interface counterAction  {
-        type: string,
-    }
+    // handlers remove any business logic from the layout code
+    const increment = () => { dispatch({ ...count, type: actionEnum.INCREMENT})}
+    const decrement = () => { dispatch({ ...count, type: actionEnum.DECREMENT}) }
+    const reset = () => { dispatch({ ...count, type: actionEnum.RESET}) }
+    const updateIncrementor = () => {dispatch({ ...count, type: actionEnum.UPDATE_INCREMENTOR, incrementor: Number(textInputRef?.current?.value)})}
 
-    const myReducer = (state: number, action: counterAction): number => {
-        switch(action.type) {
-            case 'INCREMENT':
-                return state + 1;
-            case 'DECREMENT':
-                return state - 1;
-            case 'RESET':
-                return 0;
-            default: return state;
-        }
-    }
-
-    const [count, dispatch] = useReducer(myReducer, 0)
-
-    const increment = () => { dispatch({type: 'INCREMENT'}) }
-    const decrement = () => { dispatch({type: 'DECREMENT'}) }
-    const reset = () => { dispatch({type: 'RESET'}) }
-
+    // NO business logic, only layout!
   return (
       <main>
         <div className="App container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10 w-80">
@@ -37,12 +64,18 @@ function App() {
                 Testing your counter skillz!
             </p>
         </div>
-          <div className='flex justify-center'>
+          <div className='flex justify-center mt-2'>
               <button className={buttonCss} onClick={increment}>Increment</button>
               <button className={buttonCss} onClick={reset}>Reset</button>
               <button className={buttonCss} onClick={decrement}>Decrement</button>
           </div>
-          <div className='flex justify-center mt-24 font-semibold text-8xl'>{count}</div>
+          <div className='flex justify-center mt-2'>
+              <div className='text-sm font-semibold mr-1 mt-0.5'>Incrementor:</div>
+              <div>
+                  <input value={count.incrementor} onChange={updateIncrementor} ref={textInputRef} defaultValue={1} className='border border-1 rounded w-12 pl-2' type='number' />
+              </div>
+          </div>
+          <div className='flex justify-center mt-24 font-semibold text-8xl'>{count.value}</div>
       </main>
   );
 }
